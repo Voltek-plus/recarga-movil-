8e3859a37659a79c40d978eae49674e73709b3f3
Voltek-plus <onewemas2323@gmail.com>
2025-04-24 16:38:02 +0800

    primer commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    host: "0.0.0.0",
-  }
+  // server:{
+  //   host: "0.0.0.0",
+  // },
+  build: {
+    outDir: 'dist', // Asegúrate que coincide con la configuración de Netlify
+    emptyOutDir: true
+  },
+  base: './' //
 })
